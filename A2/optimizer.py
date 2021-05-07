53236a73716a5c2ebcbe4c9b99c9bbb39109cea4
Guillaume Pavé <guillaumepave@gmail.com>
2021-05-07 11:09:13 +0200

    update with all asset

--- A2/optimizer.py
+++ A2/optimizer.py
@@ -68,7 +68,7 @@ def EV_criterion(weight,Lambda_RA,Returns_data):
     criterion=-criterion
     return criterion
 
-def Optimizer(returnData,opt='EV'):
+def Optimizer(returnData,num_col, opt='EV'):
     assert opt != "EV" or opt != 'SK'
     """
     
@@ -88,18 +88,18 @@ def Optimizer(returnData,opt='EV'):
     #starting points => 1% in each stock
     # weight for criterion :)
     # à modifier pour être utiliser selon le nombre d'asset
-    x0 = np.array([0, 0, 0])+0.01
+    x0 = np.array([x for x in range(num_col)])+0.01
     
     # constraint for weight
     cons=({'type':'eq', 'fun': lambda x:sum(x)-1})
-    Bounds= [(1e-3 , 1) for i in range(0,3)] # boudns of weights -> 0 to 1
+    Bounds= [(0 , 1) for i in range(0,num_col)] # boudns of weights -> 0 to 1
     
     Lambda_RA=3 #define teh risk aversion parameter
     
     if opt=='EV':
-        resultat = minimize(EV_criterion, x0, method='SLSQP', args=(Lambda_RA,np.array(returnData.iloc[:,0:3])),bounds=Bounds,constraints=cons,options={'disp': True})
+        resultat = minimize(EV_criterion, x0, method='SLSQP', args=(Lambda_RA,np.array(returnData.iloc[:,0:])),bounds=Bounds,constraints=cons,options={'disp': True})
     #res_SK.x: give the optimal weight
     else:
-        resultat = minimize(EV_criterion, x0, method='SLSQP', args=(Lambda_RA,np.array(returnData.iloc[:,0:])),bounds=Bounds,constraints=cons,options={'disp': True})
+        resultat = minimize(SK_criterion, x0, method='SLSQP', args=(Lambda_RA,np.array(returnData.iloc[:,0:])),bounds=Bounds,constraints=cons,options={'disp': True})
     
     return resultat
